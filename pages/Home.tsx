42610609d493aabb2ff28d07dc13a3f0725240a8
Isabela Vitek <isat12vitek@gmail.com>
2023-03-21 15:03:19 -0300

    F_00003 - Estrutura inicial tela de empresas

---
+++ pages/Home.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>O Jogo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
