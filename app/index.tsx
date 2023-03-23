558da9821845256ed6d5f59130d0255b175ee4e2
Vinicius Oliveira <viniciuscesaroliveira1@gmail.com>
2023-03-23 05:07:36 -0300

    Create login page and styles, update Readme

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>O Jogo</Text>
+      <Link href="/login">Login</Link>
       <StatusBar style="auto" />
     </View>
   );
