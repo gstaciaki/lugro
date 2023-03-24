67afc3676f2c6e5401e4f92ef04763a947847844
Guilherme Staciaki <gstaciaki99@gmail.com>
2023-03-23 21:08:37 -0300

    F_00005 - Refatoração Routers

--- app/register/user/index.tsx
+++ app/register/user/index.tsx
@@ -4,10 +4,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button  } from 'react-native';
 
 export default function Index() {
+
+  const router = useRouter()
+  const home = () => {
+    router.push({
+      pathname: "/",
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Usuario</Text>
-      
+      <Button onPress={home} title='voltar'></Button>
     </View>
   );
 }
