6929afaa4d753aa3a75d69c67cae4656897bf148
Guilherme Staciaki <gstaciaki99@gmail.com>
2023-03-28 20:34:24 -0300

    F_00008 - Estruturação Tela Description

--- app/description/index.tsx
+++ app/description/index.tsx
@@ -1,32 +1,70 @@
 import { Link, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import styles from './styles'
 
 export default function Index() {
-  const router = useRouter();
 
-  const empresa = () => {
-    router.push({
-      pathname: "/company",
-    });
-  };
+  const onClickSign = () => {
+    console.log('Eu quero')
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>
-        Decrição da Empresa
-      </Text>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.container}>
 
-      <Button onPress={empresa} title="Voltar" />
-    </View>
+        <View style={styles.imageContainer} >
+          <Image style={styles.image} source={require("../../assets/restaurant.jpg")}></Image>
+        </View>
+
+        <Text style={styles.companieName}>Nome da Empresa</Text>
+
+        <View style={styles.descriptionContainer}>
+
+          <ScrollView style={styles.textContainer}>
+            <Text style={styles.descriptionText}>Descrição de evento, cardápio (se restaurante), horários, local, valores.
+              aaa
+              aaa
+              aaa
+              aaaa
+              vfvfd
+              cfbgfdbfdbfdbd jhvfycxvdsghcvdsgcvgdsvgcsdghcvgdshcgh
+              vbfdsbvfdsvfdsvfdsv
+              vfdsvfdvsfdvfds
+              vo surta
+              bfdvfdsvfdvfdsv
+            </Text>
+          </ScrollView>
+
+          <TouchableOpacity style={styles.buttonContainer} onPress={onClickSign}>
+            <Text style={styles.buttonText}>Eu quero</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.ratingStarsContainer}>
+          <View style={styles.ratingStarsContent}>
+            <Image source={require('../../assets/stars.png')} style={styles.starImg}></Image>
+            <Text style={styles.ratingText}>(3) Avaliações</Text>
+          </View>
+        </View>
+
+        <View style={styles.commentContainer}>
+
+          <View style={styles.commentBlockOp1}>
+            <Text style={styles.commentText1}>Comentários de usuários, suas avaliações e opção para curti o comentário que eu não vo implementa agora não</Text>
+          </View>
+          <View style={styles.commentBlockOp2}>
+            <Text style={styles.commentText1}>Comentários de usuários, suas avaliações e opção para curti o comentário que eu não vo implementa agora não</Text>
+          </View>
+          <View style={styles.commentBlockOp3}>
+            <Text style={styles.commentText2}>Comentários de usuários, suas avaliações e opção para curti o comentário que eu não vo implementa agora não</Text>
+          </View>
+          <View style={styles.commentBlockOp4}>
+            <Text style={styles.commentText2}>Comentários de usuários, suas avaliações e opção para curti o comentário que eu não vo implementa agora não</Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
