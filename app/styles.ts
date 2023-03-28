83536ce53280fb6f503707c6df130b981adeea2c
Guilherme Staciaki <gstaciaki99@gmail.com>
2023-03-28 20:33:52 -0300

    F_00008 - Estilização da Tela Inicio

---
+++ app/styles.ts
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EEEFFD',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    width: '100%',
+    height: 250,
+  },
+  logo: {
+    flex: 1
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%'
+  },
+  loginButton: {
+    width: '90%',
+    height: '25%',
+    backgroundColor: '#99D14C',
+    borderRadius: 27,
+    marginBottom: 36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15
+  },
+  registerButton: {
+    width: '90%',
+    height: '25%',
+    backgroundColor: '#F9ACB3',
+    borderRadius: 27,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 33
+  }
+
+});
+
+export default styles;
