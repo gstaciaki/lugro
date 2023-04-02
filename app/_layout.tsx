41b7a37b0003eed011562eb1f3d0888732df1b03
isabela-tvitek <isat12vitek@gmail.com>
2023-04-02 15:45:03 -0300

    F_00007 - Correções

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function DefaultLayout() {
+  // return <Stack screenOptions={
+  //   {
+  //     headerShown: false
+  //   }
+  // } />;
+
   return <Stack />;
 }
--- app/event/index.tsx
+++ app/event/index.tsx
@@ -2,48 +2,49 @@ import { Link, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles'
+import ComponentEvent from './_ComponentEvent';
+
+export type EventProps = {
+    imageSource: any,
+    title: string,
+    color: string
+}
+
+const fakeEvents: EventProps[] = [
+  {
+    imageSource: require("../../assets/restaurant.jpg"),
+    title: "Evento teste bla bla bla",
+    color: '#F9ACB3',
+  },
+  {
+    imageSource: require("../../assets/restaurant.jpg"),
+    title: "Evento teste 2",
+    color: '#C6C9FF'
+  },
+  {
+    imageSource: require("../../assets/restaurant.jpg"),
+    title: "Evento teste 3",
+    color: '#252C53'
+  },
+  {
+    imageSource: require("../../assets/restaurant.jpg"),
+    title: "Evento teste 3",
+    color: '#99D14C'
+  }
+]
 
 export default function Index() {
-  const onClickSign = () => {
-    console.log('Eventos')
-  }
+  
   
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <View style={styles.container}>
         <View style={styles.commentContainer}>
 
-          <View style={styles.commentBlockOp1}>
-            <View style={styles.imageContainer} >
-              <Image style={styles.image} source={require("../../assets/restaurant.jpg")}></Image>
-            </View>
-            <View>
-              <Text style={styles.commentText1}>Evento teste bla bla bla</Text>
-            </View>
-          </View>
-
-          <View style={styles.commentBlockOp2}>
-            <View style={styles.imageContainer} >
-              <Image style={styles.image} source={require("../../assets/restaurant.jpg")}></Image>
-            </View>
-            <Text style={styles.commentText1}>Evento teste bla bla bla</Text>
-          </View>
-
-          <View style={styles.commentBlockOp3}>
-            <View style={styles.imageContainer} >
-              <Image style={styles.image} source={require("../../assets/restaurant.jpg")}></Image>
-            </View>
-            <Text style={styles.commentText2}>Evento teste bla bla bla</Text>
-          </View>
-
-          <View style={styles.commentBlockOp4}>
-            <View style={styles.imageContainer} >
-              <Image style={styles.image} source={require("../../assets/restaurant.jpg")}></Image>
-            </View>
-            <Text style={styles.commentText2}>Evento teste bla bla bla</Text>
-          </View>
-
-         
+        {fakeEvents.map((event, index) => 
+            <ComponentEvent key={index} event={event}/>
+          )}
+          
         </View>
       </View>
     </ScrollView>
