d5e5b8020f6ac543ca74773b640b759ac63de918
isabela-tvitek <isat12vitek@gmail.com>
2023-05-15 23:57:24 -0300

    F_00020 - Correções de start

---
+++ config/firebaseConfigExemplo.ts
@@ -0,0 +1,14 @@
+import { FirebaseOptions } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+export default firebaseConfig;
