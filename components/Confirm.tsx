a89991d51ace5c3f5355db6f0ced4ea140d98ba7
Ligia Falcao <lih.falcao@hotmail.com>
2023-05-21 21:04:57 -0300

    Alert

---
+++ components/Confirm.tsx
@@ -0,0 +1,56 @@
+import { View, Text, TouchableOpacity, Alert, StyleSheet } from "react-native";
+import React from "react";
+import { useRouter } from "expo-router";
+
+
+export default function Confirm(data:any) {
+  const router = useRouter();
+  const handleAlert = (data: any) => {
+    Alert.alert("Confirmar", "Tem certeza que quer fazer esse cadastro? ", [
+      {
+        text: "Cancelar",
+      },
+      {
+        text: "Sim",
+        onPress: () => {
+          console.log("Cadastrado de", data.name , "feito com sucesso");
+          router.push({
+            pathname: "../../",
+          });
+        },
+      },
+    ]);
+  };
+
+  return (
+  <TouchableOpacity style={styles.signInbutton} onPress={() => handleAlert(data)}>
+    <Text style={styles.textButton}>Cadastrar</Text>
+  </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  signInbutton: {
+    display: 'flex',
+    backgroundColor: '#99D14C',
+    borderRadius: 15,
+    width: '90%',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+    marginBottom: 30,
+    shadowColor: '#171717',
+    shadowOffset: {width: -4, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  textButton: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.1)',
+    textShadowOffset: {width: 4, height: 4},
+    textShadowRadius: 10
+  }
+});
